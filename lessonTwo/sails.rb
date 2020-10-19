8912724984966faaab805296dc8452432217e99c
Pavel <pasha.sobko.95@mail.ru>
2020-10-19 18:47:25 +0300

    done task six

--- lessonTwo/sails.rb
+++ lessonTwo/sails.rb
@@ -1,16 +1,20 @@
-backet={}
+# frozen_string_literal: true
+
+backet = {}
+result = 0
 loop do
-print "Input name of product:"
-product = gets.chomp
-break if product=="stop"
-print "Input price for one product:"
-price = gets.chomp.to_i
-print "Input number of product:"
-number = gets.chomp.to_f
-backet[product] = {price => number}
+  print 'Input name of product:'
+  product = gets.chomp
+  break if product == 'stop'
+
+  print 'Input price for one product:'
+  price = gets.chomp.to_i
+  print 'Input number of product:'
+  number = gets.chomp.to_f
+  backet[product] = { price => number }
+end
+backet.each do |_product|
+  result += price.keys.first * price.values.first
 end
 
-
-puts backet
-
-
+puts result
