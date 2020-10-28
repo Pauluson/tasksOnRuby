2cf551d5c1ea9ef580e01f44b857449cc95f9c94
Pavel <pasha.sobko.95@mail.ru>
2020-10-28 11:39:19 +0300

    all done

---
+++ lessonThree/station.rb
@@ -0,0 +1,19 @@
+class Station
+	attr_reader :name
+	def initialize(name)
+		@name = name
+		@trains = []
+	end
+	def get_train(train)
+		self.trains << train
+	def send_train(train)
+		self.trains.delete(trains)
+	end
+	def train_by_type(type)
+		self.trains.each do |train|
+			if train.type == type
+				puts  train
+			end
+		end
+	end
+end
