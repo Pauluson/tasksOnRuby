e25daa14568744d35ea46fd968dc79993abd0b7c
Pavel <pasha.sobko.95@mail.ru>
2020-11-19 01:48:58 +0300

    used rubocop

--- railway/app.rb
+++ railway/app.rb
@@ -22,24 +22,27 @@ class Menu
     @routes = []
     @trains = []
   end
+
   def main_menu
-  puts 'Welcome to railway!'
-  puts '1.Create station'
-  puts '2.Create train'
-  puts '3.Create route'
-  puts '4.Carriages'
-  puts '5.Add route to train'
-  puts '6.Fixed route'
-  puts '7.Move trains'
-  puts '0.Exit'
+    puts 'Welcome to railway!'
+    puts '1.Create station'
+    puts '2.Create train'
+    puts '3.Create route'
+    puts '4.Carriages'
+    puts '5.Add route to train'
+    puts '6.Fixed route'
+    puts '7.Move trains'
+    puts '0.Exit'
   end
+
   def create_station
-        puts 'Enter the station:'
+    puts 'Enter the station:'
     station_name = gets.chomp.to_s
     station = Station.new(station_name)
     stations << station
     puts "Your create station:#{station.name}"
   end
+
   def create_train
     puts 'Choose type of train:'
     puts '1.Passenger'
@@ -55,6 +58,7 @@ class Menu
       trains << train
     end
   end
+
   def create_route
     puts 'Choose the station:'
     show_stations
@@ -65,37 +69,40 @@ class Menu
     route = Route.new(stations[first_station - 1], stations[finish_station - 1])
     routes << route
   end
+
   def carriges
-    puts"1.Add carriage"
-    puts"2.Delete carriage"
+    puts '1.Add carriage'
+    puts '2.Delete carriage'
     choice = gets.chomp.to_i
     if choice == 1
-       puts 'On what train do you want to add carriage?'
-       show_trains
-    number_of_train = gets.chomp.to_i
-    trains.each_with_index do |train, index|
-      if (number_of_train - 1 == index) && (train.type == 'Cargo')
-        cargo_carriage = CargoCarriage.new
-        train.add_carriage(cargo_carriage)
-      elsif (number_of_train - 1 == index) && (train.type == 'Passenger')
-        passenger_carriage = PassengerCarriage.new
-        train.add_carriage(passenger_carriage)
+      puts 'On what train do you want to add carriage?'
+      show_trains
+      number_of_train = gets.chomp.to_i
+      trains.each_with_index do |train, index|
+        if (number_of_train - 1 == index) && (train.type == 'Cargo')
+          cargo_carriage = CargoCarriage.new
+          train.add_carriage(cargo_carriage)
+        elsif (number_of_train - 1 == index) && (train.type == 'Passenger')
+          passenger_carriage = PassengerCarriage.new
+          train.add_carriage(passenger_carriage)
+        end
       end
-    end
     elsif choice == 2
       puts 'On what train do you want to delete carriage?'
       show_trains
-    number_of_train = gets.chomp.to_i
-    trains.each_with_index do |train, index|
-      next unless number_of_train - 1 == index
-      train.carriages.each do |carriage|
-        train.delete_carriage(carriage)
+      number_of_train = gets.chomp.to_i
+      trains.each_with_index do |train, index|
+        next unless number_of_train - 1 == index
+
+        train.carriages.each do |carriage|
+          train.delete_carriage(carriage)
+        end
       end
     end
   end
-end
-def add_route
-  puts 'Enter the train to add route:'
+
+  def add_route
+    puts 'Enter the train to add route:'
     show_trains
     number_of_train = gets.chomp.to_i
     puts 'Choice route:'
@@ -103,11 +110,13 @@ def add_route
     number_of_route = gets.chomp.to_i
     trains.each_with_index do |train, index|
       next unless number_of_train - 1 == index
+
       routes.each_with_index do |value, index|
         train.route = value if number_of_route - 1 == index
       end
     end
   end
+
   def fixed_route
     puts 'Choose the route to fixed:'
     show_routes
@@ -138,8 +147,9 @@ def add_route
       end
     end
   end
+
   def move_trains
-     stations.each do |station|
+    stations.each do |station|
       puts "Station:#{station.name}, Train:#{station.all_trains}"
     end
     puts 'Choose the number of train:'
@@ -149,7 +159,7 @@ def add_route
     puts '2.Move previous station'
     choice = gets.chomp.to_i
     if choice == 1
-      trains.each_with_index do |train,index|
+      trains.each_with_index do |train, index|
         train.move_next_station if number_of_train - 1 == index
       end
     elsif choice == 2
@@ -158,26 +168,30 @@ def add_route
       end
     end
   end
+
   def information
     p stations
     p routes
     p trains
-
   end
+
   private
+
   def show_stations
-        stations.each_with_index do |station, index|
+    stations.each_with_index do |station, index|
       puts "#{index + 1}.#{station.name}"
     end
   end
+
   def show_routes
-     routes.each_with_index do |route, index|
+    routes.each_with_index do |route, index|
       puts "#{index + 1}.#{route}"
     end
   end
+
   def show_trains
-    trains.each_with_index do |train,index|
-    puts "#{index + 1}.#{train.number_of_train},#{train.type}"
+    trains.each_with_index do |train, index|
+      puts "#{index + 1}.#{train.number_of_train},#{train.type}"
     end
   end
 end
